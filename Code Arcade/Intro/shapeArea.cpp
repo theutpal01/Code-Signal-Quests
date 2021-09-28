8ac7d16a889b80155915d33451acf71cd526da6a
Utpal <theutpal01@gmail.com>
2021-09-28 15:19:29 +0530

    C++ Porgram Solutions

---
+++ Code Arcade/Intro/shapeArea.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int shapeArea(int n)
+{
+    return (pow(n, 2) - pow((n - 1), 2));
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << shapeArea(n);
+    return 0;
+}
